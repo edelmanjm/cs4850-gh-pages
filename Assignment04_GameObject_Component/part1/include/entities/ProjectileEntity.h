8a3a123bb2c52cafdfdc185212c31ba03629d583
edelmanjm <edelmanjm@users.noreply.github.com>
2024-02-02 20:55:03 -0500

    Migrate from assignment 3

---
+++ Assignment04_GameObject_Component/part1/include/entities/ProjectileEntity.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <entities/GameEntity.h>
+
+class ProjectileEntity : public GameEntity {
+
+private:
+    float m_Speed{200.0f};
+    bool m_IsFiring{false};
+    bool m_YDirectionUp{true};
+    uint64_t timeSinceLastLaunch;
+
+public:
+    explicit ProjectileEntity(Sprite sprite);
+
+    void Launch(float x, float y, bool yDirectionIsUp, uint64_t minLaunchTime = 3000);
+
+    void Update(float deltaTime) override;
+
+    void Render(SDL_Renderer *renderer) override;
+};
