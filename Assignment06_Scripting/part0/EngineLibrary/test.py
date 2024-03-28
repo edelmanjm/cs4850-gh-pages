fbd976561154bf31d548f7a877d32f4fbf76d330
edelmanjm <edelmanjm@users.noreply.github.com>
2024-03-28 04:53:01 -0400

    Add serve randomness and direction

--- Assignment06_Scripting/part0/EngineLibrary/test.py
+++ Assignment06_Scripting/part0/EngineLibrary/test.py
@@ -1,3 +1,4 @@
+import random
 from enum import Enum
 import math
 from typing import List, NamedTuple
@@ -12,7 +13,11 @@ class Pong:
         l: int
         r: int
 
-    class Direction(Enum):
+    class XDirection(Enum):
+        LEFT = 0
+        RIGHT = 1
+
+    class YDirection(Enum):
         UP = 0
         DOWN = 1
 
@@ -54,7 +59,7 @@ class Pong:
         self.top = rose.SDL_FRect(0, -1, self.w, 1)
         self.bottom = rose.SDL_FRect(0, self.h + 1, self.w, 1)
 
-        self.reset()
+        self.reset(self.XDirection.RIGHT)
 
         self.score = Pong.Score(0, 0)
 
@@ -63,10 +68,16 @@ class Pong:
         self.application = rose.Application(renderer)
         self.application.set_scene(self.scene)
 
-    def reset(self):
-        self.ball.set_position(rose.SDL_FRect(self.w / 2 - self.ball_size / 2, self.h / 2 - self.ball_size / 2,
+    def reset(self, serve_direction: XDirection):
+        ball_y = random.randint(int(self.h / 3), int(self.h * 2 / 3))
+        self.ball.set_position(rose.SDL_FRect(self.w / 2 - self.ball_size / 2, ball_y,
                                               self.ball_size, self.ball_size))
-        self.ball.set_velocity(self.ball_speed, 0)
+        x_multiplier: float
+        if serve_direction == self.XDirection.LEFT:
+            x_multiplier = -1.0
+        else:
+            x_multiplier = 1.0
+        self.ball.set_velocity(self.ball_speed * x_multiplier, (random.random() * 2 - 1) * self.ball_speed * 0.25)
 
         self.paddle_l.set_position(rose.SDL_FRect(self.paddle_x, self.paddle_y, self.paddle_w, self.paddle_h))
         self.paddle_r.set_position(rose.SDL_FRect(self.w - self.paddle_x - self.paddle_w, self.paddle_y,
@@ -75,21 +86,21 @@ class Pong:
     def on_input(self, delta_time: float, keys: List[int]):
         # TODO either import the SDL_Scancode enum, or find an equivalent in python
         if keys[81]:
-            self.move_paddle(delta_time, self.paddle_r, self.Direction.UP)
+            self.move_paddle(delta_time, self.paddle_r, self.YDirection.UP)
         elif keys[82]:
-            self.move_paddle(delta_time, self.paddle_r, self.Direction.DOWN)
+            self.move_paddle(delta_time, self.paddle_r, self.YDirection.DOWN)
 
     def ai(self, delta_time: float):
         paddle = self.paddle_l
         difference = self.ball.get_transform().y - paddle.get_transform().y
         if difference > 4:
-            self.move_paddle(delta_time, paddle, self.Direction.UP)
+            self.move_paddle(delta_time, paddle, self.YDirection.UP)
         elif difference < -4:
-            self.move_paddle(delta_time, paddle, self.Direction.DOWN)
+            self.move_paddle(delta_time, paddle, self.YDirection.DOWN)
 
-    def move_paddle(self, delta_time: float, paddle, direction: Direction):
+    def move_paddle(self, delta_time: float, paddle, direction: YDirection):
         transform = paddle.get_transform()
-        if direction == self.Direction.UP:
+        if direction == self.YDirection.UP:
             new_y = min(transform.y + self.paddle_speed * delta_time, self.h - self.paddle_h)
             paddle.set_position(rose.SDL_FRect(transform.x, new_y, transform.w, transform.h))
         else:
@@ -114,10 +125,12 @@ class Pong:
     def on_update(self, delta_time: float):
         if rose.SDL_FRect.intersects(self.ball.get_transform(), self.left):
             self.score = Pong.Score(self.score.l + 1, self.score.r)
-            self.reset()
+            # Winner serves
+            self.reset(self.XDirection.RIGHT)
         elif rose.SDL_FRect.intersects(self.ball.get_transform(), self.right):
             self.score = Pong.Score(self.score.l, self.score.r + 1)
-            self.reset()
+            # Winner serves
+            self.reset(self.XDirection.LEFT)
         elif (rose.SDL_FRect.intersects(self.ball.get_transform(), self.top)
               or rose.SDL_FRect.intersects(self.ball.get_transform(), self.bottom)):
             prev_velocity = self.ball.get_velocity()
